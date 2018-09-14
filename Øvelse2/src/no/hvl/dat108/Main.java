b97ea6bf4fa47ddf84201451b24c4fc61b213960
571730 <571730@stud.hvl.no>
2018-09-14 15:25:24 +0200

    Svar oppgave d)

--- Øvelse2/src/no/hvl/dat108/Main.java
+++ Øvelse2/src/no/hvl/dat108/Main.java
@@ -1,6 +1,10 @@
 package no.hvl.dat108;
 
 public class Main {
+    /**
+     * Starter opp noen konsumere og producere og setter dem i gang
+     * @param args ikke i bruk
+     */
     public static void main(String[] args){
         Buffer buffer = new Buffer();
         Consumer cons = new Consumer(buffer);
@@ -10,6 +14,8 @@ public class Main {
         cons.start();
         cons2.start();
 
+        // Svar oppgave d)
+        // println() burde vaere synkronisert ja, saa ikke forskjellige traader prover aa printe ut samtidig
 
 
     }
